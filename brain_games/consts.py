d9cc946382d504bbce784608222f0a7479263373
oleg <fatfry@mail.ru>
2023-10-09 21:58:09 +0300

    fix removed brain-games

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -1,11 +1,8 @@
 PROGRESSION_LENGTH = 12
-
 BRAIN_CALC_INSTRUCTION = 'What is the result of the expression?'
-
-EVEN_QUESTION_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
-
+EVEN_QUESTION_INSTRUCTION = ('Answer "yes" if the number is even,'
+                             ' otherwise answer "no".')
 BRAIN_GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
-
-BRAIN_PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-
+BRAIN_PRIME_INSTRUCTION = ('Answer "yes" if given number is prime. '
+                           'Otherwise answer "no".')
 BRAIN_PG_INSTRUCTION = 'What number is missing in the progression?'
--- brain_games/engine.py
+++ brain_games/engine.py
@@ -12,6 +12,7 @@ def start_game(get_question_and_answer, instruction):
         if result == user_input:
             print('Correct!')
         else:
-            print(f"'{user_input}' is wrong answer ;(. Correct answer was '{result}'.\nLet's try again, {name}!")
+            print(f"'{user_input}' is wrong answer ;(. "
+                  f"Correct answer was '{result}'.\nLet's try again, {name}!")
             return
     print(f"Congratulations, {name}!")
