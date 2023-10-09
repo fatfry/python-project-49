80f1d7f73d08a2ce0e382a178e7de45dbf2a627e
oleg <fatfry@mail.ru>
2023-10-09 15:25:56 +0300

    fix consts.py

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -6,6 +6,6 @@ EVEN_QUESTION_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answe
 
 BRAIN_GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
 
-BRAIN_PRIME_INSTRUCTION = 'Answer "yes" if the number is prime, otherwise answer "no".'
+BRAIN_PRIME_INSTRUCTION: str = 'Answer "yes" if the number is prime, otherwise answer "no".'
 
 BRAIN_PG_INSTRUCTION = 'What number is missing in the progression?'
