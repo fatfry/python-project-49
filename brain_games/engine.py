f41aec108dc888d812fa7268fdfa8903f530c6e1
oleg <fatfry@mail.ru>
2023-10-22 12:14:34 +0300

    fix  engine.py, brain-pg and README.md

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,8 +3,7 @@ from brain_games.consts import MAX_ATTEMPTS
 
 
 def start_game(get_question_and_answer, instruction):
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name?')
+    name = prompt.string('Welcome to the Brain Games!/nMay I have your name?')
     print(f'Hello, {name}\n{instruction}')
     for _ in range(MAX_ATTEMPTS):
         question, result = get_question_and_answer()
@@ -14,6 +13,7 @@ def start_game(get_question_and_answer, instruction):
             print('Correct!')
         else:
             print(f"'{user_input}' is wrong answer ;(. "
-                  f"Correct answer was '{result}'.\nLet's try again, {name}!")
+                  f"Correct answer was '{result}'.")
+            print(f"Let's try again, {name}!")
             return
     print(f"Congratulations, {name}!")
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,14 +6,16 @@ from brain_games.utils import get_random_num
 from brain_games.consts import BRAIN_PG_INSTRUCTION
 
 
+def create_progression(first_num, diff, missed_num_ind, progression_length):
+    return ' '.join(['..' if i == missed_num_ind else str(first_num + i * diff)
+                     for i in range(progression_length)])
+
+
 def brain_progression():
     first_num, diff = get_random_num(), get_random_num()
     missed_num_ind = randint(0, PROGRESSION_LENGTH - 1)
-    pg = ' '.join(['..' if i == missed_num_ind else str(first_num + i * diff)
-                   for i in range(PROGRESSION_LENGTH)
-                   ])
+    question = create_progression(first_num, diff, missed_num_ind, PROGRESSION_LENGTH)
     result = str(first_num + missed_num_ind * diff)
-    question = pg
     return question, result
 
 
